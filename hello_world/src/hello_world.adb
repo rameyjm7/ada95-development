b8665c3186809434e74281bd88d7cc37b7673430
Jacob M. Ramey <rameyjm7@gmail.com>
2024-04-12 18:06:34 -0400

    added reference

--- hello_world/src/hello_world.adb
+++ hello_world/src/hello_world.adb
@@ -1,5 +1,7 @@
 with Ada.Text_IO;
 
+-- https://learn.adacore.com/courses/intro-to-ada/chapters/imperative_language.html#hello-world
+
 procedure hello_world is
 begin
    --  Print "Hello, World!" to the screen
