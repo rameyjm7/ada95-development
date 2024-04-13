b6c7ada00de9e9c4d7a59c05847248b841cda1c6
Jacob M. Ramey <rameyjm7@gmail.com>
2024-04-12 23:19:04 -0400

    updated to show nested subprogram

--- subprogram/src/test_subprogram.adb
+++ subprogram/src/test_subprogram.adb
@@ -1,39 +1,26 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Increment_By;
 
-procedure show_increment is
+procedure Show_Increment is
    A, B, C : Integer;
+
+   procedure Display_Result is
+   begin
+      Put_Line ("Increment of "
+                & Integer'Image (A)
+                & " with "
+                & Integer'Image (B)
+                & " is "
+                & Integer'Image (C));
+   end Display_Result;
+
 begin
-   C := Increment_By;
-   --              ^ Parameterless call,
-   --                value of I is 0
-   --                and Incr is 1
-
-   Put_Line ("Using defaults for Increment_By is "
-             & Integer'Image (C));
-
    A := 10;
    B := 3;
    C := Increment_By (A, B);
-   --                 ^ Regular parameter passing
-
-   Put_Line ("Increment of "
-             & Integer'Image (A)
-             & " with "
-             & Integer'Image (B)
-             & " is "
-             & Integer'Image (C));
-
+   Display_Result;
    A := 20;
    B := 5;
-   C := Increment_By (I    => A,
-                      Incr => B);
-    --                ^ Named parameter passing
-
-   Put_Line ("Increment of "
-             & Integer'Image (A)
-             & " with "
-             & Integer'Image (B)
-             & " is "
-             & Integer'Image (C));
-end show_increment;
+   C := Increment_By (A, B);
+   Display_Result;
+end Show_Increment;
